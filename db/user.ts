27503b0ee3e27c40713219d87688c98ebef0039c
Acbox <acbox0328@gmail.com>
2025-06-24 18:43:01 +0800

    chore: put some api need to implements

--- db/user.ts
+++ db/user.ts
@@ -1,9 +1,12 @@
 import { pgTable, text, timestamp, uuid, json, numeric } from "drizzle-orm/pg-core";
 import { sql } from "drizzle-orm";
 
+const infinityTo2099 = sql`timestamp '2099-12-31 23:59:59'`
+
 export const table = pgTable("user", {
   id: text("id").primaryKey().notNull(),
   plan: text("plan").notNull().default('free'),
+  expire: timestamp("expire").notNull().default(infinityTo2099),
   credits: numeric("credits").notNull().default('0'),
   resources: json("resources").notNull().default([]),
   available_models: json("available_models").notNull().default([]),
