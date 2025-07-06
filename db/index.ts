bc8cd9cc83dbeb63137f960835196650aa6c41c5
Acbox <acbox0328@gmail.com>
2025-07-07 02:26:16 +0800

    ci: add vercel preset

--- db/index.ts
+++ db/index.ts
@@ -1,7 +1,6 @@
 import 'dotenv/config'
 import { drizzle } from 'drizzle-orm/node-postgres'
 import { DATABASE_URL } from '@/utils'
-import * as schema from './schema'
 
 const db = drizzle(DATABASE_URL);
 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,7 +24,7 @@ export default defineNuxtConfig({
   // },
   css: ['~/assets/css/main.css'],
   nitro: {
-    // preset: 'vercel'
+    preset: 'vercel'
   },
   // hub: {
 
