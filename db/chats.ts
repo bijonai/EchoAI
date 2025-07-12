682bac1a118dec272e1b7735f54c4fb1717d792b
Acbox <acbox0328@gmail.com>
2025-07-13 03:51:18 +0800

    refactor(db): refactor the structure of table `chats`

--- db/chats.ts
+++ db/chats.ts
@@ -2,23 +2,15 @@ import { sql } from "drizzle-orm";
 import { pgTable, text, timestamp, uuid, json } from "drizzle-orm/pg-core";
 
 export const chats = pgTable("chats", {
-  id: uuid("chat_id").primaryKey().default(sql`gen_random_uuid()`),
+  id: uuid("id").primaryKey().default(sql`gen_random_uuid()`),
   uid: text("uid").notNull(),
   created_at: timestamp("created_at").notNull().defaultNow(),
   updated_at: timestamp("updated_at").notNull().defaultNow(),
   context: json("context").notNull().default([]),
-  board: json("board").notNull().default({}),
-  branches: json("branches").notNull().default([]),
-  designer_context: json("designer_context").notNull().default([]),
-  designer_results: json("designer_results").notNull().default([]),
-  speaker_context: json("speaker_context").notNull().default([]),
-  speaker_results: json("speaker_results").notNull().default([]),
-  layout_context: json("layout_context").notNull().default([]),
-  layout_results: json("layout_results").notNull().default([]),
-  chalk_context: json("chalk_context").notNull().default([]),
-  chalk_results: json("chalk_results").notNull().default([]),
-  resource: json("resource").notNull().default({
-    knowledge: []
-  }),
+  status: text("status").notNull().default('init'),
+  pages: json("pages").notNull().default([]),
+  messages: json("messages").notNull().default([]),
+  design: json("design").notNull().default({ root: [] }),
+  title: text("title").notNull().default(''),
 });
 
